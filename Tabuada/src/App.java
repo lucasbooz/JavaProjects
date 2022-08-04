84c3c6f18a24ce379262b2f54c7d0e7382cd0df0
lucasbooz <lucasrbooz@gmail.com>
2022-08-04 09:46:18 -0300

    Criado algorítmo para construção de tabuada

--- Tabuada/src/App.java
+++ Tabuada/src/App.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (Scanner sc = new Scanner(System.in)) {
+            char proc;
+            int n = 0;
+            System.out.println("Deseja executar o algorítmo?[s/n]");
+            proc = sc.next().charAt(0);
+            while ((proc == 'S') || (proc == 's')) {
+                System.out.println("Informe uma tabuada: ");
+                n = sc.nextInt();
+                for (int i = 0; i <= 10; i++) {
+                    System.out.println(n + " x " + i + " = " + i * n);
+                }
+                System.out.println("Deseja executar novamente?[s/n]");
+                proc = sc.next().charAt(0);
+            }
+        }
+        System.out.println("Obrigado por usar o sistema!");
     }
 }
