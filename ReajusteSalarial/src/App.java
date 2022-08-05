8decaac102ba0eaf8d48c1c887c28771dad992f6
lucasbooz <lucasrbooz@gmail.com>
2022-08-05 16:38:15 -0300

    Criação de loop

--- ReajusteSalarial/src/App.java
+++ ReajusteSalarial/src/App.java
@@ -4,12 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         float salarioAtual, aumento;
+        char proc;
 
-        System.out.print("Informe o salário: ");
-        salarioAtual = sc.nextFloat();
+        System.out.println("Deseja executar o programa?[s/n]");
+        proc = sc.next().charAt(0);
 
-        aumento = salarioAtual * 1.3f;
+        while ((proc == 'S') || (proc == 's')) {
 
-        System.out.println(salarioAtual + " +30% = " + aumento);
+            System.out.print("Informe o salário: ");
+            salarioAtual = sc.nextFloat();
+
+            aumento = salarioAtual * 1.3f;
+
+            System.out.println(salarioAtual + " +30% = " + aumento);
+            System.out.println("Deseja executar novamente?[s/n]");
+            proc = sc.next().charAt(0);
+        }
     }
 }
