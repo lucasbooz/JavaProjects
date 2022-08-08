3ad12cb9a9400f2507fe2ba8479edc35353562c4
lucasbooz <lucasrbooz@gmail.com>
2022-08-08 11:12:50 -0300

    add try into Scanner

--- ReajusteSalarial/src/App.java
+++ ReajusteSalarial/src/App.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        float salarioAtual, aumento, reajuste;
-        char proc;
+        try (Scanner sc = new Scanner(System.in)) {
+            float salarioAtual, aumento, reajuste;
+            char proc;
 
-        System.out.println("Deseja executar o programa?[s/n]");
-        proc = sc.next().charAt(0);
-
-        while ((proc == 'S') || (proc == 's')) {
-
-            System.out.print("Informe o salário: ");
-            salarioAtual = sc.nextFloat();
-
-            System.out.print("Informe o aumento(%): ");
-            aumento = sc.nextInt();
-
-            reajuste = ((aumento / 100)) + 1;
-
-            System.out.println(salarioAtual + "+" + aumento + "%=" + reajuste * salarioAtual);
-            System.out.println("Deseja executar novamente?[s/n]");
+            System.out.println("Deseja executar o programa?[s/n]");
             proc = sc.next().charAt(0);
+
+            while ((proc == 'S') || (proc == 's')) {
+
+                System.out.print("Informe o salário: ");
+                salarioAtual = sc.nextFloat();
+
+                System.out.print("Informe o aumento(%): ");
+                aumento = sc.nextInt();
+
+                reajuste = ((aumento / 100)) + 1;
+
+                System.out.println(salarioAtual + "+" + aumento + "%=" + reajuste * salarioAtual);
+                System.out.println("Deseja executar novamente?[s/n]");
+                proc = sc.next().charAt(0);
+            }
         }
     }
 }
