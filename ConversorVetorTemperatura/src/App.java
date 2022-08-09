68fa7db3d3c7b638e027809f355a2b9b9d831ef7
lucasbooz <lucasrbooz@gmail.com>
2022-08-09 09:51:21 -0300

    teste branch

--- ConversorVetorTemperatura/src/App.java
+++ ConversorVetorTemperatura/src/App.java
@@ -10,30 +10,33 @@ public class App {
         float celsius, fahrenheit;
         char proc;
 
-        do {
-            montaMenu.montaMenu();
-
-            switch (montaMenu.option) {
-                case 1:
-
-                    System.out.println("Informe a temperatura em Celsius(ºC): ");
-                    celsius = sc.nextFloat();
-                    System.out.println("Temperatura em Fahrenheit(ºF): " + ((celsius * 1.8f) + 32) + "ºF.");
-                    break;
-                case 2:
-                    System.out.println("Informe a temperatura em Fahrenheit(ºF): ");
-                    fahrenheit = sc.nextFloat();
-                    System.out.println("Temperatura em Celsius(ºC): " + ((fahrenheit - 32) / 1.8) + "ºC.");
-                    break;
-                case 3:
-                    System.out.println("Fórmula ºC -> ºF: F = (C * 1.8) + 32");
-                    System.out.println("Fórmula ºF -> ºC: C = (F - 32) / 1.8");
-                    break;
-            }
-        } while (montaMenu.option != 4);
-        System.out.println("Deseja executar novamente?[s/n]");
+        System.out.println("Deseja executar o sistema?[s/n]");
         proc = sc.next().charAt(0);
+        while ((proc == 'S') || (proc == 's')) {
+            do {
+                montaMenu.montaMenu();
 
+                switch (montaMenu.option) {
+                    case 1:
+
+                        System.out.println("Informe a temperatura em Celsius(ºC): ");
+                        celsius = sc.nextFloat();
+                        System.out.println("Temperatura em Fahrenheit(ºF): " + ((celsius * 1.8f) + 32) + "ºF.");
+                        break;
+                    case 2:
+                        System.out.println("Informe a temperatura em Fahrenheit(ºF): ");
+                        fahrenheit = sc.nextFloat();
+                        System.out.println("Temperatura em Celsius(ºC): " + ((fahrenheit - 32) / 1.8) + "ºC.");
+                        break;
+                    case 3:
+                        System.out.println("Fórmula ºC -> ºF: F = (C * 1.8) + 32");
+                        System.out.println("Fórmula ºF -> ºC: C = (F - 32) / 1.8");
+                        break;
+                }
+            } while (montaMenu.option != 4);
+            System.out.println("Deseja executar novamente?[s/n]");
+            proc = sc.next().charAt(0);
+        }
         System.out.println("Obrigado por usar o conversor!");
     }
 }
